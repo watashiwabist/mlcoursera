e23446f42c3c78d8671dbfe22595fe24f2afc31c
watashiwabist <bulatgaea@gmail.com>
2020-06-30 01:23:35 +0300

    titanic

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+
+data = pd.read_csv('titanic.csv')
+data = data.drop(['PassengerId', 'Name', 'SibSp', 'Parch',
+                  'Ticket', 'Cabin', 'Embarked'], axis=1)
+label = LabelEncoder()
+dicts = {}
+label.fit(data.Sex.drop_duplicates())
+dicts['Sex'] = list(label.classes_)
+data.Sex = label.transform(data.Sex)
+
+X = data[['Pclass', 'Fare', 'Age', 'Sex']]
+X = pd.get_dummies(X)
+X = X.fillna({'Age': X.Age.mean()})
+y = data.Survived
+
+clf = tree.DecisionTreeClassifier(random_state=241)
+clf.fit(X, y)
+importances = pd.DataFrame({'features': X.columns, 'feature_importances': clf.feature_importances_}).sort_values('feature_importances', ascending=False)
+
+print(importances)
