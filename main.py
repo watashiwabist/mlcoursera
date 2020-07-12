d67a1eceb129dd665d3bc02aae35a3ea1377a94f
watashiwabist <bulatgaea@gmail.com>
2020-07-13 02:23:09 +0300

    Learned how to predict payday

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import KFold, cross_val_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import scale
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Ridge
+from scipy.sparse import hstack
 
-df = pd.read_csv('wine.data', header=None,
-                 names=['Class', 'Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols',
-                        'Flavanoids',
-                        'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue',
-                        'OD280/OD315 of diluted wines', 'Proline'])
-kfold = KFold(n_splits=5, shuffle=True, random_state=42)
-X = scale(df.drop('Class', axis=1))
-y = df.Class
-for k in range(1, 51):
-    knn = KNeighborsClassifier(n_neighbors=k)
-    res = cross_val_score(knn, X, y, cv=kfold)
-    print(np.mean(res), k)
+df_train = pd.read_csv('salary-train.csv')
+df_test = pd.read_csv('salary-test-mini.csv')
+
+X_train = df_train.drop(['SalaryNormalized'], axis=1)
+X_train['FullDescription'] = X_train['FullDescription'].replace('[^a-zA-Z0-9]', ' ', regex=True)
+X_train = X_train.apply(lambda x: x.astype(str).str.lower())
+y_train = df_train['SalaryNormalized']
+
+X_test = df_test.drop(['SalaryNormalized'], axis=1)
+y_test = df_test['SalaryNormalized']
+
+X_train['LocationNormalized'].fillna('nan', inplace=True)
+X_train['ContractTime'].fillna('nan', inplace=True)
+
+tfid = TfidfVectorizer(min_df=5)
+X_train_vect = tfid.fit_transform(X_train['FullDescription'])
+X_test_vect = tfid.transform(X_test['FullDescription'])
+
+enc = DictVectorizer()
+X_train_categ = enc.fit_transform(X_train[['LocationNormalized', 'ContractTime']].to_dict('records'))
+X_test_categ = enc.transform(X_test[['LocationNormalized', 'ContractTime']].to_dict('records'))
+
+X_for_train = hstack([X_train_vect, X_train_categ])
+X_for_test = hstack([X_test_vect, X_test_categ])
+
+clf = Ridge(alpha=1)
+clf.fit(X_for_train, y_train)
+print(clf.predict(X_for_test))
